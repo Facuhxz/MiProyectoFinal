5be139ab708f685c4da7033be22cc5b2db41c539
Facundo Lopez <flopezpistan@gmail.com>
2024-06-08 22:31:24 -0300

    Segunda y tercer views

--- EntregaFinal/urls.py
+++ EntregaFinal/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from EntregaFinal.views import inicio
+from AppsDePag.views import inicio, template1, template2, template3
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", inicio)
+    path("", inicio),
+    path("template1/<str:nombre>/<str:apellido>/<int:edad>", template1),
+    path("template2/<str:nombre>/<str:apellido>/<int:edad>", template2),
+    path("template3/<str:nombre>/<str:apellido>/<int:edad>", template3),
+    
 ]
